3724a4c8b597b3a669050e55580f5b0d22cbd186
patrick mcdowell <112023765+ngrok-patrick@users.noreply.github.com>
2022-10-01 22:25:37 -0700

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -O https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm.tgz
+tar xvfz ngrok-v3-stable-linux-arm.tgz 
+./ngrok config add-authtoken $APIKEY
+cp ./ngrok /usr/bin
